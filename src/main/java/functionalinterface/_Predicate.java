69dd157a5222266c6929ce26001152aaaeced5bb
Andrew-Zed <andrew.awadyke@gmail.com>
2023-01-11 11:16:21 +0100

    6th commit, Predicate is boolean

---
+++ src/main/java/functionalinterface/_Predicate.java
@@ -0,0 +1,28 @@
+package functionalinterface;
+
+import java.util.function.Predicate;
+
+public class _Predicate {
+    public static void main(String[] args) {
+
+        System.out.println("Without Predicate");
+        System.out.println(isPhoneNumberValid("07000000000"));
+        System.out.println(isPhoneNumberValid("0700000000"));
+        System.out.println(isPhoneNumberValid("09009877300"));
+
+        System.out.println("With Predicate");
+        System.out.println(isPhoneNumberValidPredicate.test("07000000000"));
+        System.out.println(isPhoneNumberValidPredicate.test("0700000000"));
+        System.out.println(isPhoneNumberValidPredicate.test("09009877300"));
+
+    }
+
+    static boolean isPhoneNumberValid(String phoneNumber) {
+        return phoneNumber.startsWith("07") && phoneNumber.length() == 11;
+    }
+
+    static Predicate<String> isPhoneNumberValidPredicate = phoneNumber ->
+                   phoneNumber.startsWith("07") && phoneNumber.length() == 11;
+
+
+}
