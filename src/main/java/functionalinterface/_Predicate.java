bfd73f706b5711ef60c9abcbca70fbaf68daf35f
Andrew-Zed <andrew.awadyke@gmail.com>
2023-01-11 11:26:55 +0100

    6th B commit, Predicate takes argument and return a boolean

--- src/main/java/functionalinterface/_Predicate.java
+++ src/main/java/functionalinterface/_Predicate.java
@@ -15,6 +15,16 @@ public class _Predicate {
         System.out.println(isPhoneNumberValidPredicate.test("0700000000"));
         System.out.println(isPhoneNumberValidPredicate.test("09009877300"));
 
+        System.out.println(
+                "Is phone number valid and contains number 3 " +
+                isPhoneNumberValidPredicate.and(containsNumber3).test("09009877300")
+        );
+
+        System.out.println(
+                "Is phone number valid and contains number 3 " +
+                        isPhoneNumberValidPredicate.or(containsNumber3).test("07000000000")
+        );
+
     }
 
     static boolean isPhoneNumberValid(String phoneNumber) {
@@ -23,6 +33,11 @@ public class _Predicate {
 
     static Predicate<String> isPhoneNumberValidPredicate = phoneNumber ->
                    phoneNumber.startsWith("07") && phoneNumber.length() == 11;
+
+    static Predicate<String> containsNumber3 = phoneNumber ->
+            phoneNumber.contains("3");
+
+
 
 
 }
